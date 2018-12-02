ecbf0747595ff803b99d7dd48b0fb25cbe9bdc18
SIMRAN1 <simranpreetkaurkhalsa13@gmail.com>
2018-12-02 10:17:04 +0530

    Adding pipe image on popover

--- rampup/controller/RampPickerVC.swift
+++ rampup/controller/RampPickerVC.swift
@@ -11,6 +11,7 @@ import SceneKit
 
 class RampPickerVC: UIViewController {
     
+
     var sceneView: SCNView!
     var size: CGSize!
     
@@ -28,7 +29,21 @@ class RampPickerVC: UIViewController {
         view.frame = CGRect(origin: CGPoint.zero, size: size)
         sceneView = SCNView(frame: CGRect(x: 0, y: 0, width: size.width, height: size.height))
         view.insertSubview(sceneView, at: 0)
+        let scene = SCNScene(named: "art.scnassets/ramps.scn")!
+        sceneView.scene = scene
+        let camera = SCNCamera()
+        camera.usesOrthographicProjection =  true
+        scene.rootNode.camera = camera
+        
+        
+        
+        let obj  = SCNScene(named: "art.scnassets/pyramid.dae")!
+        let node = obj.rootNode.childNode(withName: "pyramid", recursively: true)!
+        node.scale = SCNVector3Make(0.0042, 0.0042, 0.0042)
+        node.position = SCNVector3Make(-1, 0.7, -1)
+        scene.rootNode.addChildNode(node)
         preferredContentSize = size
+        
         // Do any additional setup after loading the view.
     }
     
